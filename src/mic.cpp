923f18bdf5a863dc09cee2aee17d605e13febec2
ixaxaar <root@ixaxaar.in>
2024-03-11 20:48:25 +0530

    read from mic and write to sd card

--- src/mic.cpp
+++ src/mic.cpp
@@ -11,64 +11,61 @@
 #define AUDIO_BUFFER_SIZE                                                      \
   (MAX_SECONDS_TO_BUFFER * SAMPLE_RATE * CHANNELS * BITS_PER_SAMPLE / 8)
 
+#define PIN_AUDIO_KIT_SD_CARD_CS 13
+#define PIN_AUDIO_KIT_SD_CARD_MISO 2
+#define PIN_AUDIO_KIT_SD_CARD_MOSI 15
+#define PIN_AUDIO_KIT_SD_CARD_CLK 14
+
+// I2SStream i2sStream;
+// auto cfg = i2sStream.defaultConfig(RX_MODE);
+// cfg.i2s_format = I2S_STD_FORMAT; // optional because default setting
+// cfg.bits_per_sample = 32;
+// cfg.channels = 2; // optional because default setting
+// cfg.sample_rate = 44100;
+// cfg.is_master = true; // optional because default setting
+// i2sStream.begin(cfg);
+
 MicManager::MicManager(int bckPin, int wsPin, int dataPin)
   : _bckPin(bckPin)
   , _wsPin(wsPin)
   , _dataPin(dataPin)
-  , _audioBuffer(nullptr)
-  , _bufferSize(0)
-  , _recordedSize(0)
   , _isRecording(false)
 {
-  _i2sConfig = { .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
-                 .sample_rate = SAMPLE_RATE,
-                 .bits_per_sample = i2s_bits_per_sample_t(BITS_PER_SAMPLE),
-                 .channel_format = CHANNELS == 1 ? I2S_CHANNEL_FMT_ONLY_RIGHT
-                                                 : I2S_CHANNEL_FMT_RIGHT_LEFT,
-                 .communication_format = I2S_COMM_FORMAT_I2S_MSB,
-                 .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-                 .dma_buf_count = 8,
-                 .dma_buf_len = 64,
-                 .use_apll = false,
-                 .tx_desc_auto_clear = false,
-                 .fixed_mclk = 0 };
 
-  _pinConfig = { .bck_io_num = _bckPin,
-                 .ws_io_num = _wsPin,
-                 .data_out_num = I2S_PIN_NO_CHANGE,
-                 .data_in_num = _dataPin };
+  cfg = i2sStream.defaultConfig(RX_MODE);
+
+  cfg.i2s_format = I2S_STD_FORMAT; // optional because default setting
+  cfg.bits_per_sample = 32;
+  cfg.channels = 1; // optional because default setting
+  cfg.sample_rate = 16000;
+  cfg.is_master = true; // optional because default setting
+
+  cfg.pin_bck = _bckPin;
+  cfg.pin_ws = _wsPin;
+  cfg.pin_data = _dataPin; // output
 }
 
 void
-MicManager::begin()
+MicManager::startRecording(String fileName)
 {
-  i2s_driver_install(I2S_NUM_0, &_i2sConfig, 0, NULL);
-  i2s_set_pin(I2S_NUM_0, &_pinConfig);
-  i2s_set_clk(I2S_NUM_0, SAMPLE_RATE, BITS_PER_SAMPLE, I2S_CHANNEL_MONO);
-}
 
-void
-MicManager::deinitializeI2S()
-{
-  i2s_driver_uninstall(I2S_NUM_0);
-}
+  SPI.begin(PIN_AUDIO_KIT_SD_CARD_CLK,
+            PIN_AUDIO_KIT_SD_CARD_MISO,
+            PIN_AUDIO_KIT_SD_CARD_MOSI,
+            PIN_AUDIO_KIT_SD_CARD_CS);
 
-void
-MicManager::setCallback(AudioBufferCallback callback)
-{
-  _callback = callback;
-}
-
-void
-MicManager::startRecording()
-{
-  if (_audioBuffer == nullptr) {
-    _audioBuffer = static_cast<uint8_t*>(malloc(AUDIO_BUFFER_SIZE));
+  if (!SD.begin(PIN_AUDIO_KIT_SD_CARD_CS)) {
+    printf("SD card failed to initialize");
+    return;
   }
-  _recordedSize = 0;
-  _isRecording = true;
 
-  // Start background task for audio collection
+  file = SD.open(fileName, FILE_WRITE);
+  file.seek(0);
+  copier.setCheckAvailableForWrite(false);
+  i2sStream.begin(cfg);
+  copier.begin(file, i2sStream);
+
+  _isRecording = true;
   xTaskCreate(audioTaskWrapper, "AudioCollectTask", 2048, this, 1, NULL);
 }
 
@@ -76,24 +73,11 @@ void
 MicManager::stopRecording()
 {
   _isRecording = false;
-  // Give some time for the audioTask to exit cleanly before freeing resources
   vTaskDelay(pdMS_TO_TICKS(100));
-  if (_audioBuffer != nullptr) {
-    free(_audioBuffer);
-    _audioBuffer = nullptr;
-  }
-}
 
-char*
-MicManager::getRecordedAudio()
-{
-  return reinterpret_cast<char*>(_audioBuffer);
-}
-
-size_t
-MicManager::getRecordedAudioSize()
-{
-  return _recordedSize;
+  copier.end();
+  i2sStream.end();
+  file.close();
 }
 
 void
@@ -106,39 +90,7 @@ MicManager::audioTaskWrapper(void* param)
 void
 MicManager::audioTask()
 {
-  size_t bytesRead;
   while (_isRecording) {
-    // Determine how much space is left in the buffer
-    size_t remainingSpace = AUDIO_BUFFER_SIZE - _recordedSize;
-
-    uint8_t tempBuffer[512];
-
-    // Read only as much as can fit in the remaining space of the buffer
-    size_t toRead = sizeof(tempBuffer) <= remainingSpace ? sizeof(tempBuffer)
-                                                         : remainingSpace;
-    i2s_read(I2S_NUM_0, tempBuffer, toRead, &bytesRead, portMAX_DELAY);
-
-    if (bytesRead > 0) {
-      // Ensure we don't read beyond the buffer size
-      if (_recordedSize + bytesRead > AUDIO_BUFFER_SIZE) {
-        bytesRead = AUDIO_BUFFER_SIZE - _recordedSize;
-      }
-
-      // Copy the data into the main audio buffer
-      memcpy(_audioBuffer + _recordedSize, tempBuffer, bytesRead);
-      _recordedSize += bytesRead;
-
-      // Check if the buffer is half full or more
-      if (_recordedSize >= AUDIO_BUFFER_SIZE / 2) {
-        // Trigger the callback if set
-        if (_callback) {
-          _callback();
-        }
-
-        // Reset the buffer and its size after the callback has been triggered
-        memset(_audioBuffer, 0, AUDIO_BUFFER_SIZE);
-        _recordedSize = 0;
-      }
-    }
+    copier.copy();
   }
 }
--- src/mic.h
+++ src/mic.h
@@ -1,6 +1,8 @@
 #ifndef MIC_MANAGER_HPP
 #define MIC_MANAGER_HPP
 
+#include "AudioTools.h"
+#include "SD.h"
 #include "driver/i2s.h"
 #include <Arduino.h>
 
@@ -10,27 +12,22 @@ class MicManager
 {
 public:
   MicManager(int bckPin, int wsPin, int dataPin);
-  void begin();
-  void startRecording();
+  void startRecording(String fileName);
   void stopRecording();
-  char* getRecordedAudio();
-  size_t getRecordedAudioSize();
-  void setCallback(AudioBufferCallback callback);
 
   static void audioTaskWrapper(void* param);
 
 private:
   int _bckPin, _wsPin, _dataPin;
-  i2s_config_t _i2sConfig;
-  i2s_pin_config_t _pinConfig;
-  uint8_t* _audioBuffer;
-  size_t _bufferSize;
-  size_t _recordedSize;
   bool _isRecording;
-  AudioBufferCallback _callback;
 
-  void initializeI2S();
-  void deinitializeI2S();
+  I2SStream i2sStream;
+  I2SConfig cfg;
+  StreamCopy copier;
+  File file;
+
+  // void initializeI2S();
+  // void deinitializeI2S();
   void audioTask();
 };
 
