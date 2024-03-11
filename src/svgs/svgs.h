675b47a01ff016ae5e36fc5ae972c41ff1d1c4f9
ixaxaar <root@ixaxaar.in>
2024-03-11 15:24:13 +0530

    add images

---
+++ src/svgs/svgs.h
@@ -0,0 +1,104 @@
+// Master header file including all bitmap headers
+#include "1F600.h"
+// #include "1F601.h"
+// #include "1F602.h"
+// #include "1F603.h"
+#include "1F604.h"
+// #include "1F605.h"
+// #include "1F606.h"
+// #include "1F607.h"
+// #include "1F609.h"
+// #include "1F610.h"
+// #include "1F611.h"
+// #include "1F612.h"
+// #include "1F613.h"
+// #include "1F614.h"
+// #include "1F615.h"
+// #include "1F616.h"
+// #include "1F617.h"
+// #include "1F618.h"
+// #include "1F619.h"
+// #include "1F620.h"
+// #include "1F622.h"
+// #include "1F623.h"
+// #include "1F624.h"
+// #include "1F625.h"
+// #include "1F626.h"
+// #include "1F627.h"
+// #include "1F628.h"
+// #include "1F629.h"
+// #include "1F630.h"
+// #include "1F631.h"
+// #include "1F632.h"
+// #include "1F633.h"
+// #include "1F634.h"
+// #include "1F635-200D-1F4AB.h"
+// #include "1F635.h"
+// #include "1F637.h"
+// #include "1F641.h"
+// #include "1F642.h"
+// #include "1F644.h"
+// #include "1F910.h"
+// #include "1F911.h"
+// #include "1F913.h"
+#include "1F914.h"
+// #include "1F917.h"
+#include "1F920.h"
+// #include "1F922.h"
+// #include "1F923.h"
+// #include "1F924.h"
+// #include "1F927.h"
+#include "1F928.h"
+// #include "1F929.h"
+
+const unsigned short* image_grinning = image_1F600;
+// const unsigned short* image_grin = image_1F601;
+// const unsigned short* image_joy = image_1F602;
+// const unsigned short* image_smiley = image_1F603;
+const unsigned short* image_smiley = image_1F604;
+// const unsigned short* image_sweat_smile = image_1F605;
+// const unsigned short* image_laughing = image_1F606;
+// const unsigned short* image_innocent = image_1F607;
+// const unsigned short* image_wink = image_1F609;
+// const unsigned short* image_neutral_face = image_1F610;
+// const unsigned short* image_expressionless = image_1F611;
+// const unsigned short* image_unamused = image_1F612;
+// const unsigned short* image_sweat = image_1F613;
+// const unsigned short* image_pensive = image_1F614;
+// const unsigned short* image_confused = image_1F615;
+// const unsigned short* image_confounded = image_1F616;
+// const unsigned short* image_kissing = image_1F617;
+// const unsigned short* image_kissing_heart = image_1F618;
+// const unsigned short* image_kissing_smiling_eyes = image_1F619;
+// const unsigned short* image_angry = image_1F620;
+// const unsigned short* image_cry = image_1F622;
+// const unsigned short* image_persevere = image_1F623;
+// const unsigned short* image_triumph = image_1F624;
+// const unsigned short* image_disappointed_relieved = image_1F625;
+// const unsigned short* image_frowning = image_1F626;
+// const unsigned short* image_anguished = image_1F627;
+// const unsigned short* image_fearful = image_1F628;
+// const unsigned short* image_weary = image_1F629;
+// const unsigned short* image_cold_sweat = image_1F630;
+// const unsigned short* image_scream = image_1F631;
+// const unsigned short* image_astonished = image_1F632;
+// const unsigned short* image_flushed = image_1F633;
+// const unsigned short* image_sleeping = image_1F634;
+// const unsigned short* image_dizzy_face = image_1F635_200D_1F4AB;
+// const unsigned short* image_dead_face = image_1F635;
+// const unsigned short* image_mask = image_1F637;
+// const unsigned short* image_slightly_frowning_face = image_1F641;
+// const unsigned short* image_slightly_smiling_face = image_1F642;
+// const unsigned short* image_face_with_rolling_eyes = image_1F644;
+// const unsigned short* image_zipper_mouth_face = image_1F910;
+// const unsigned short* image_money_mouth_face = image_1F911;
+// const unsigned short* image_nerd_face = image_1F913;
+const unsigned short* image_thinking_face = image_1F914;
+// const unsigned short* image_hugging_face = image_1F917;
+const unsigned short* image_cowboy_hat_face = image_1F920;
+// const unsigned short* image_nauseated_face = image_1F922;
+// const unsigned short* image_rofl = image_1F923;
+// const unsigned short* image_drooling_face = image_1F924;
+// const unsigned short* image_sneezing_face = image_1F927;
+const unsigned short* image_face_with_raised_eyebrow = image_1F928;
+// const unsigned short* image_starstruck = image_1F929;
